8aca850e4d0c13bfe0a3c07afa86862cdcb2794d
Suman Roy <lifeofsroy@gmail.com>
2023-11-29 18:54:04 +0530

    updated

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,8 @@ self.addEventListener("install", function (event) {
 
 const filesToCache = [
     '/',
-    '/offline.html'
+    '/offline.html',
+    '/notfound.html',
 ];
 
 const checkResponse = function (request) {
